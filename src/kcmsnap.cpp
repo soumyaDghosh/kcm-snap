fc383c220e8e9ef63aa52f7d289e870d1bece631
Soumyadeep Ghosh <soumyadeepghosh2004@zohomail.in>
2024-08-12 22:28:49 +0530

    kcmsnap: improve the backend with pointers & qobject

--- src/kcmsnap.cpp
+++ src/kcmsnap.cpp
@@ -1,24 +1,47 @@
 #include "kcmsnap.h"
  
-KCMSnap::KCMSnap(QSnapdSnap* &snap, const QList<QSnapdPlug*> &plugs, const QList<QSnapdSlot*> &slots)
+KCMSnap::KCMSnap(QSnapdSnap* snap, const QList<QSnapdPlug*> plugs, const QList<QSnapdSlot*> slots)
     : m_snap(snap), 
     m_plugs(plugs), 
     m_slots(slots)
-{
-
-}
+{}
  
 QSnapdSnap* KCMSnap::snap() const 
-{ 
+{
     return m_snap; 
 }
- 
-const QList<QSnapdPlug*>& KCMSnap::plugs() const 
+
+const QList<QSnapdPlug*> KCMSnap::plugs() const 
 { 
     return m_plugs; 
 }
  
-const QList<QSnapdSlot*>& KCMSnap::slots() const 
+const QList<QSnapdSlot*> KCMSnap::slots() const 
 { 
     return m_slots; 
 }
+
+const QString KCMSnap::title() const
+{
+    QString title = m_snap->name();
+    title[0] = title[0].toUpper();
+    for (int i = 1; i < title.length(); ++i) {  
+        if (title[i - 1] == QStringLiteral("-")) {
+            title[i - 1] = QChar::Space;
+            title[i] = title[i].toUpper();
+        }
+    }
+    return title;
+}
+
+// QImage KCMSnap::icon() const
+// {
+//     QSnapdClient client;
+//     QSnapdGetIconRequest* reqIcon = client.getIcon(m_snap->icon());
+//     reqIcon->runSync();
+//     QBuffer buffer;
+//     buffer.setData(reqIcon->icon()->data());
+//     QImageReader reader(&buffer);
+//     QImage icon = reader.read();
+//     return icon;
+// }
--- src/kcmsnap.h
+++ src/kcmsnap.h
@@ -5,16 +5,25 @@
 #include <Snapd/Plug>
 #include <Snapd/Slot>
  
-class KCMSnap
+class KCMSnap : public QObject
 {
+    Q_OBJECT
+    Q_PROPERTY(QSnapdSnap* snap READ snap CONSTANT)
+    Q_PROPERTY(QList<QSnapdPlug *> plugs READ plugs CONSTANT)
+    Q_PROPERTY(QList<QSnapdSlot *> slots READ slots CONSTANT)
+    Q_PROPERTY(QString title READ title CONSTANT)
+    //Q_PROPERTY(QImage icon READ icon CONSTANT)
+
     public:
-        KCMSnap(QSnapdSnap* &snap, const QList<QSnapdPlug*>& plugs, const QList<QSnapdSlot*>& slots);
+        KCMSnap(QSnapdSnap* snap, const QList<QSnapdPlug*> plugs, const QList<QSnapdSlot*> slots);
         QSnapdSnap* snap() const;
-        const QList<QSnapdPlug*>& plugs() const;
-        const QList<QSnapdSlot*>& slots() const;
+        const QList<QSnapdPlug*> plugs() const;
+        const QList<QSnapdSlot*> slots() const;
+        const QString title() const;
+        //QImage icon() const;
  
     private:
-        QPointer<QSnapdSnap> m_snap;
+        QSnapdSnap* m_snap;
         QList<QSnapdPlug *> m_plugs;
         QList<QSnapdSlot *> m_slots;
 };
